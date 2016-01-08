fb0bafbbc08fccdd59b6eb4b367456d9afb6f729
SteveKunG <mccommander_minecraft@hotmail.com>
2016-01-08 10:33:13 +0700

    Fix version check link

--- src/main/java/stevekung/mods/moreplanets/core/ThreadVersionCheckMP.java
+++ src/main/java/stevekung/mods/moreplanets/core/ThreadVersionCheckMP.java
@@ -12,12 +12,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent.Serializer;
 import net.minecraft.util.StatCollector;
 import stevekung.mods.moreplanets.core.config.ConfigManagerMP;
 import stevekung.mods.moreplanets.core.util.MPLog;
-import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
 
@@ -57,7 +57,7 @@ public class ThreadVersionCheckMP extends Thread
         {
             try
             {
-                URL url = new URL("https://raw.githubusercontent.com/SteveKunG/More-Planets/master/version.txt");
+                URL url = new URL("https://raw.githubusercontent.com/SteveKunG/More-Planets/1.7.10/version.txt");
                 HttpURLConnection http = (HttpURLConnection) url.openConnection();
                 http.addRequestProperty("User-Agent", "Mozilla/4.76");
                 BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
@@ -84,7 +84,7 @@ public class ThreadVersionCheckMP extends Thread
 
                             if (sideToCheck.equals(Side.CLIENT))
                             {
-                                FMLClientHandler.instance().getClient().thePlayer.addChatMessage(Serializer.func_150699_a("[{\"text\":\"New version available for \",\"extra\":[{\"text\":\"" + EnumChatFormatting.AQUA + "More Planets!\"},{\"text\":\"" + EnumChatFormatting.GREEN + " v" + String.valueOf(remoteMajVer) + "." + String.valueOf(remoteMinVer) + "." + String.valueOf(remoteBuildVer) + " \"},{\"text\":\"" + EnumChatFormatting.RED + "[CLICK HERE]\",\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"" + EnumChatFormatting.YELLOW + "Download Latest Version\"},\"clickEvent\":{\"action\":\"open_url\",\"value\":\"" + this.URL + "\"}}]}]"));
+                                Minecraft.getMinecraft().thePlayer.addChatMessage(Serializer.func_150699_a("[{\"text\":\"New version available for \",\"extra\":[{\"text\":\"" + EnumChatFormatting.AQUA + "More Planets!\"},{\"text\":\"" + EnumChatFormatting.GREEN + " v" + String.valueOf(remoteMajVer) + "." + String.valueOf(remoteMinVer) + "." + String.valueOf(remoteBuildVer) + " \"},{\"text\":\"" + EnumChatFormatting.RED + "[CLICK HERE]\",\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"" + EnumChatFormatting.YELLOW + "Download Latest Version\"},\"clickEvent\":{\"action\":\"open_url\",\"value\":\"" + this.URL + "\"}}]}]"));
                             }
                             else if (sideToCheck.equals(Side.SERVER))
                             {
